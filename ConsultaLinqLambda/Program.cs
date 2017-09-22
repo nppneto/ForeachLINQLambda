2cc8b48961d469af458a44345c7c8cb543915026
hawk5 <hawk5@DESKTOP-PF66N0S>
2017-09-22 02:06:54 -0300

    Estudo LINQ e Lambda.

--- ConsultaLinqLambda/Program.cs
+++ ConsultaLinqLambda/Program.cs
@@ -10,6 +10,38 @@ namespace ConsultaLinqLambda
     {
         static void Main(string[] args)
         {
+            List<string> cidades = new List<string>();
+
+            cidades.Add("São Paulo");
+            cidades.Add("São Bernardo");
+            cidades.Add("Santos");
+            cidades.Add("São Caetano");
+            cidades.Add("Praia Grande");
+            cidades.Add("São Vicente");
+
+            BuscaPrimeiroComLambda(cidades, "Sã").ForEach(x => Console.WriteLine(x));
+
+            Console.ReadKey();
+        }
+
+        public static string BuscaPrimeiroComForeach(List<string> lista, string termo)
+        {
+            foreach (string item in lista)
+            {
+                if (item.Equals(termo))
+                    return item;
+            }
+            return "Não encontrado.";
+        }
+
+        public static List<string> BuscaPrimeiroComLinq(List<string> lista, string termo)
+        {
+            return (from item in lista where item.Contains(termo) select item).ToList();
+        }
+
+        public static List<string> BuscaPrimeiroComLambda(List<string> lista, string termo)
+        {
+            return lista.Where(x => x.Contains(termo)).ToList();
         }
     }
 }
